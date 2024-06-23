9468854ff486299628e035a85be16425d17eb797
felliperfm <felliperfm@gmail.com>
2024-06-23 20:16:14 -0300

    "Tarefa 3 concluída"

---
+++ src/scripts/albumsDatabase.js
@@ -0,0 +1,44 @@
+export const albumList = [
+    {
+        title: "Acabou o Chorare",
+        genre: "MPB",
+        band: "Novos Baianos",
+        price: "137.91",
+        img: "./src/assets/imgs/Acabou_o_Chorare.png",
+    },
+    {
+        title: "The Wall",
+        genre: "Rock",
+        band: "Pink Floyd",
+        price: "123.56",
+        img: "./src/assets/imgs/The_Wall.png",
+    },
+    {
+        title: "To Pimp a Butterfly",
+        genre: "Rap",
+        band: "Kendrick Lamar",
+        price: "102.66",
+        img: "./src/assets/imgs/To_Pimp_a_Butterfly.png",
+    },
+    {
+        title: "Thriller",
+        genre: "Pop",
+        band: "Michael Jackson",
+        price: "142.32",
+        img: "./src/assets/imgs/Thriller.jpg",
+    },
+    {
+        title: "All Eyez on Me",
+        genre: "Hip-Hop",
+        band: "2Pac",
+        price: "50.13",
+        img: "./src/assets/imgs/All_Eyez_on_Me.jpg",
+    },
+    {
+        title: "Dom de Sambar",
+        genre: "Samba",
+        band: "Turma do Pagode",
+        price: "51.67",
+        img: "./src/assets/imgs/Dom_de_Sambar.jpg",
+    },
+];
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,6 @@
+import { applyInputRangeStyle } from "./inputRange.js"
+import { albumList } from "./albumsDatabase.js";
+
 document.querySelectorAll('.genreButton').forEach(button => {
     button.addEventListener('click', () => {
         document.querySelectorAll('.genreButton').forEach(btn => btn.classList.remove('selected'));
@@ -6,3 +9,61 @@ document.querySelectorAll('.genreButton').forEach(button => {
 
     });
 });
+
+function routine() {
+    applyInputRangeStyle()
+}
+
+routine()
+
+function renderAlbums(albums) {
+    const albumsList = document.querySelector('.albums__list');
+    albumsList.innerHTML = '';
+
+    albums.forEach(album => {
+        const albumItem = document.createElement('li');
+        albumItem.classList.add('album');
+
+        const albumImage = document.createElement('img');
+        albumImage.classList.add('album__image');
+        albumImage.src = album.img;
+        albumItem.appendChild(albumImage);
+
+        const albumDetail = document.createElement('div');
+        albumDetail.classList.add('album-detail');
+        albumItem.appendChild(albumDetail);
+
+        const albumTitle = document.createElement('h3');
+        albumTitle.classList.add('title-3', 'album-detail__title');
+        albumTitle.textContent = album.title;
+        albumDetail.appendChild(albumTitle);
+
+        const albumSubtitle = document.createElement('div');
+        albumSubtitle.classList.add('album-subtitle');
+        albumDetail.appendChild(albumSubtitle);
+
+        const bandParagraph = document.createElement('p');
+        bandParagraph.textContent = album.band;
+        albumSubtitle.appendChild(bandParagraph);
+
+        const genreParagraph = document.createElement('p');
+        genreParagraph.textContent = album.genre;
+        albumSubtitle.appendChild(genreParagraph);
+
+        const albumPrice = document.createElement('div');
+        albumPrice.classList.add('album-price');
+        albumDetail.appendChild(albumPrice);
+
+        const priceParagraph = document.createElement('p');
+        priceParagraph.textContent = `R$ ${album.price}`;
+        albumPrice.appendChild(priceParagraph);
+
+        const buyButton = document.createElement('button');
+        buyButton.textContent = 'Comprar';
+        albumPrice.appendChild(buyButton);
+
+        albumsList.appendChild(albumItem);
+    });
+}
+
+renderAlbums(albumList);
